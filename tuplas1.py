bf5e4d362d52a5537f46b53411f7e94fff7c8b3f
Hiagoca <hiagoca.senac@gmail.com>
2024-07-31 19:06:32 -0300

    Delete tuplas1.py

--- tuplas1.py
+++
@@ -1,8 +0,0 @@
-minha_tupla = ("vermelho", "laranja", "amarelo", "verde", "azul", "anil", "violeta")
-print(minha_tupla[3])
-x = list(minha_tupla)
-x[4] = "preto"
-minha_tupla = tuple(x)
-print(minha_tupla)
-cores_invertidas = ("violeta", "anil", "azul", "verde", "Amarelo", "laranja", "vermelho")
-print(cores_invertidas)
